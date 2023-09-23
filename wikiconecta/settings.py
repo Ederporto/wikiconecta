bfc168fd7f6bd86d2de4395e5143eb3a3be4d47c
Éder Porto <eder.porto@hotmail.com>
2023-09-23 03:44:38 -0300

    fixing bugs

--- wikiconecta/settings.py
+++ wikiconecta/settings.py
@@ -17,7 +17,6 @@ from django.utils.translation import gettext_lazy as _
 # SECURITY WARNING: keep the secret key used in production secret!
 from .settings_local import *
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +40,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -169,10 +170,8 @@ LOCALE_PATHS = (
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
